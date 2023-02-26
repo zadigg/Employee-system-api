c97cd02e1d8b35bd8972211678292aa11d28e7aa
zadigg <abelkibebe5@gmail.com>
2023-02-25 23:10:16 -0600

    Cors Solved

--- src/main/java/com/employeesystem/employeesystemapi/controller/EmployeeController.java
+++ src/main/java/com/employeesystem/employeesystemapi/controller/EmployeeController.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
